e91c152fa8158a175e4944ac4b6738877fd60c08
wedgles97 <wedegles@gmail.com>
2022-07-17 21:38:30 -0300

    Fonts, StatusBar e outros ajustes

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import { Heading, VStack } from "native-base"
+import React from "react"
+
+export function SignIn() {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+    </VStack>
+  )
+}
