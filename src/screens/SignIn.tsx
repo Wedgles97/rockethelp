4792d51eec188f34137a730b4973c7a1ba5c9a7e
wedgles97 <wedegles@gmail.com>
2022-07-17 23:39:19 -0300

    Terminado a tela de login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,36 @@
-import { Heading, VStack } from "native-base"
-import React from "react"
+import { Heading, VStack, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+
+import { Input } from '../Components/Input';
+import { Button } from '../Components/Button';
 
 export function SignIn() {
+
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
+      <Input 
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4} />}
+        
+      />
+      <Input 
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4} />}
+        secureTextEntry
+      />
+
+      <Button title="Entrar" w="full" />
 
     </VStack>
   )
